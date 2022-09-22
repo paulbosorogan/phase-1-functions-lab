a38a411804360984546b82375b40fa106af7d896
Paul Bosorogan <bosorogan.paul@gmail.com>
2022-09-22 19:38:20 -0400

    commit tree f25e47afc2e398dc1b52af768c38514e68a220ea

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUp){
+let distanceInBlocks = pickUp - 42;
+    if (pickUp<=42){
+        distanceInBlocks = 42 - pickUp; }
+return distanceInBlocks;
+}
+
+function distanceFromHqInFeet(pickUp){
+let feetCount = distanceFromHqInBlocks(pickUp) * 264;
+return feetCount;
+}
+
+function distanceTravelledInFeet(start, destination) {
+let feetTravelled = (destination - start);
+    if (start > destination){
+        feetTravelled = start - destination;
+    }
+return feetTravelled* 264;
+}
+
+function calculatesFarePrice(start, destination){
+let feetMeter = distanceTravelledInFeet (start, destination);
+    if (feetMeter<= 400){
+        return 0;
+    }
+    if (feetMeter < 2000){
+        let fare = feetMeter - 400;
+        return fare * 0.02;
+    }
+    if (feetMeter < 2500){
+        return 25;
+    }
+    if (feetMeter > 2500){
+        return 'cannot travel that far';
+    }
+}
